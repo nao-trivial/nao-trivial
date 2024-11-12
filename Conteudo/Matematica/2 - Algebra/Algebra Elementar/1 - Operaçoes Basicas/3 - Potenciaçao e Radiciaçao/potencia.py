aa4a909f28c64684431379ab9558063c2a172a42
Hassan Santos Primo <hassanprimo500@gmail.com>
2024-11-12 10:27:51 -0300

    save

--- Conteudo/Matematica/2 - Algebra/Algebra Elementar/1 - Operaçoes Basicas/3 - Potenciaçao e Radiciaçao/potencia.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("forneca um numero inteiro que sera elevado a n-esima potencial: "))
-nesima = int(input(f"Forneça o valor de n conforme {numero}^n: "))
-
-
-for i in range(nesima):
-    numero *= numero
-    
-print(numero)
