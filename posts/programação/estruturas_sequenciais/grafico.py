74350d90f3027bee24ddfccb15bd97859fe60f8e
Hassan Santos Primo <151403813+nao-trivial@users.noreply.github.com>
2025-04-15 18:43:24 -0300

    Criar estruturas_sequenciais/grafico.py

---
+++ posts/programação/estruturas_sequenciais/grafico.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [2, 4, 6, 8]
+
+plt.plot(x, y)
+plt.title("Gráfico Simples")
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+plt.grid(True)
+plt.show()
