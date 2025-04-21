9b7a8b724d1a16c3096eda9c96c6aa694c3a77f6
Hassan Santos Primo <hassanprimo500@gmail.com>
2025-04-20 22:23:44 -0300

    analise do mercado

--- ebook/teoria-do-caos/iteracao_simples.py
+++ ebook/teoria-do-caos/iteracao_simples.py
@@ -1,3 +1,5 @@
 x = 1000  # Valor inicial (ex: dinheiro, população)
 historico = [x] + [x := 0.7 * x + 50 for _ in range(20)]  # Dinâmica: x_{n+1} = 0.7x_n + 50
 print(f"EVOLUÇÃO: {historico}")  # Trajetória do sistema
+
+
